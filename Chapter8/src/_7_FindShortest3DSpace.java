3baab87a5f8704b8196e9c805fcbbfd35ee8a940
Aonozan <Aonozan@Aonozan-PC>
2016-04-18 10:41:06 +0200

    Jos par zadataka od jutros...

---
+++ Chapter8/src/_7_FindShortest3DSpace.java
@@ -0,0 +1,33 @@
+
+public class _7_FindShortest3DSpace {
+	public static double distance(double[] p1, double[] p2){
+		return Math.sqrt(Math.pow((p2[0] - p1[0]), 2) + Math.pow((p2[1] - p1[1]), 2) + Math.pow((p2[2] - p1[2]), 2));
+	}
+	public static void main(String[] args) {
+		double[][] points = {
+				{-1, 0, 3},
+				{-1, -1, -1},
+				{4, 1, 1},
+				{2, 0.5, 9},
+				{3.5, 2, -1},
+				{3, 1.5, 3},
+				{-1.5, 4, 2},
+				{5.5, 4, -0.5}};
+		int p1 = 0;
+		int p2 = 1;
+		double ShortestDistance = distance(points[0], points[1]);
+		for (int i = 0; i < points.length; i++) {
+			for (int j = i+1; j < points.length; j++) {
+				double distance = distance(points[i], points[j]);
+				if (distance < ShortestDistance) {
+					ShortestDistance = distance;
+					p1 = i+1;
+					p2 = j+1;
+				}
+				//System.out.printf("Distance of p%d and p%d is %.3f\n", i+1, j+1, distance);
+			}
+		}
+		System.out.printf("\n\nThe closest two points are p%d and p%d.\nDistance is %.3f\n", p1, p2, ShortestDistance);
+	}
+
+}
---
+++ Chapter8/src/_8_FindAllShortest2DSpace.java
@@ -0,0 +1,32 @@
+
+public class _8_FindAllShortest2DSpace {
+	public static double distance(double[] p1, double[] p2){
+		return Math.sqrt((p2[0] - p1[0]) * (p2[0] - p1[0]) + (p2[1] - p1[1]) * (p2[1] - p1[1]));
+	}
+	public static void main(String[] args) {
+		double[][] points = {
+				{0, 0},
+				{1, 1},
+				{-1, -1},
+				{2, 2},
+				{-2, -2},
+				{-3, -3},
+				{-4, -4},
+				{5, 5}};
+		double ShortestDistance = -1;
+		String printString = "";
+		for (int i = 0; i < points.length; i++) {
+			for (int j = i+1; j < points.length; j++) {
+				double distance = distance(points[i], points[j]);
+				if (distance < ShortestDistance || ShortestDistance == -1) {
+					ShortestDistance = distance;
+					printString = "The clossest two points are ("+points[i][0]+", "+points[i][0]+") and ("+points[j][0]+", "+points[j][0]+")\n";
+				}else if (distance == ShortestDistance) {
+					printString += "The clossest two points are ("+points[i][0]+", "+points[i][0]+") and ("+points[j][0]+", "+points[j][0]+")\n";
+				}
+			}
+		}
+		System.out.printf(printString+"\nAnd their distance is %.4f.", ShortestDistance);
+	}
+
+}
